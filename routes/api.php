28650ff089d9ea3704d40094511dfb0441fad622
shayanahmad1999 <shayanahmad235@gmail.com>
2025-08-13 13:24:41 +0500

    routes for user added

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,34 @@
 <?php
 
+use App\Http\Controllers\Api\AuthenticatedSessionController;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\RegisteredUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+// ---------- user api code (route) in one pattern ---------- //
+Route::controller(RegisterController::class)->group(function () {
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [RegisterController::class, 'destroy']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+// ---------- user api code (route) in other pattern ---------- //
+Route::post('/user-register', [RegisteredUserController::class, 'store']);
+Route::post('/user-login', [AuthenticatedSessionController::class, 'store']);
+
+Route::get('/user-user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/user-logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth:sanctum');
+
