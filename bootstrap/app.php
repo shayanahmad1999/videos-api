d80c9b1f8b97c8bb2e0ebaced261c3b7ec2be514
shayanahmad1999 <shayanahmad235@gmail.com>
2025-08-13 13:23:39 +0500

    default unautorized middleware created

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,28 @@
 <?php
 
+use App\Http\Middleware\ForceJsonResponse;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // for single
+        $middleware->api(prepend: [
+            ForceJsonResponse::class,
+        ]);
+        // for group
+        // $middleware->group('api', [
+        //     // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        //     // 'throttle:api',
+        //     ForceJsonResponse::class,
+        // ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
